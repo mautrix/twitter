f7c7f59b87107bb4ce411b4b2b9a9c2105be6c9a
Carl Ambroselli <git@carl-ambroselli.de>
2022-03-06 16:04:47 +0100

    Bridge bot should always join rooms to support displaying read receipts

--- mautrix_twitter/portal.py
+++ mautrix_twitter/portal.py
@@ -908,6 +908,8 @@ class Portal(DBPortal, BasePortal):
             },
         ]
         invites = []
+        if self.is_direct:
+            invites.append(self.az.bot_mxid)
         if self.config["bridge.encryption.default"] and self.matrix.e2ee:
             self.encrypted = True
             initial_state.append(
@@ -916,8 +918,6 @@ class Portal(DBPortal, BasePortal):
                     "content": {"algorithm": "m.megolm.v1.aes-sha2"},
                 }
             )
-            if self.is_direct:
-                invites.append(self.az.bot_mxid)
         if self.encrypted or self.private_chat_portal_meta or not self.is_direct:
             name = self.name
 
