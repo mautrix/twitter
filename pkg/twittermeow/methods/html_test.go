174448ed56005b5e0c982ec5f5489fabb9d8c0c4
Nick Mills-Barrett <nick@beeper.com>
2026-08-04 16:19:51 +0100

    Remove test that actually queries twitter

--- pkg/twittermeow/methods/html_test.go
+++ pkg/twittermeow/methods/html_test.go
@@ -1,11 +1,8 @@
 package methods
 
 import (
-	"io"
-	"net/http"
 	"strings"
 	"testing"
-	"time"
 )
 
 func TestParseDocumentCookieAssignments(t *testing.T) {
@@ -40,57 +37,5 @@ func TestParseCastleBootstrapInfo(t *testing.T) {
 	}
 	if strings.Contains(gotURL, "ondemand.s") {
 		t.Fatalf("ParseOndemandCastleURLFromScript() = %q, want Castle chunk URL", gotURL)
-	}
-}
-
-func TestParseOndemandSURLFromScript(t *testing.T) {
-	client := &http.Client{Timeout: 20 * time.Second}
-	req, err := http.NewRequest(http.MethodGet, "https://x.com/", nil)
-	if err != nil {
-		t.Fatalf("failed to create request: %v", err)
-	}
-	req.Header.Set("User-Agent", "Mozilla/5.0")
-	req.Header.Set("Accept-Language", "en-US,en;q=0.9")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		t.Skipf("failed to fetch x.com: %v", err)
-	}
-	defer resp.Body.Close()
-
-	html, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatalf("failed to read x.com response: %v", err)
-	}
-
-	ondemandURL := ParseOndemandSURLFromScript(html)
-	if ondemandURL == "" {
-		mainScriptURL := ParseMainScriptURL(string(html))
-		if mainScriptURL == "" {
-			t.Fatalf("failed to locate main script URL from x.com response")
-		}
-
-		req, err = http.NewRequest(http.MethodGet, mainScriptURL, nil)
-		if err != nil {
-			t.Fatalf("failed to create main script request: %v", err)
-		}
-		req.Header.Set("User-Agent", "Mozilla/5.0")
-		req.Header.Set("Accept-Language", "en-US,en;q=0.9")
-
-		resp, err = client.Do(req)
-		if err != nil {
-			t.Fatalf("failed to fetch main script: %v", err)
-		}
-		defer resp.Body.Close()
-
-		script, err := io.ReadAll(resp.Body)
-		if err != nil {
-			t.Fatalf("failed to read main script response: %v", err)
-		}
-		ondemandURL = ParseOndemandSURLFromScript(script)
-	}
-
-	if ondemandURL == "" {
-		t.Fatalf("failed to resolve ondemand.s URL from live x.com bootstrap")
 	}
 }
